17161b5902b0bcd791d2c0713b172ed9edd4cc80
Llewellyn Lovell <ll@locoblox.co>
2017-05-29 01:26:50 -0700

    update comments

--- client/app.js
+++ client/app.js
@@ -5,6 +5,10 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 
+/**
+ * Rendering
+*/
+
 ReactDOM.render(
   <h1>Hello from client</h1>, // eslint-disable-line react/jsx-filename-extension
   document.getElementById('app'),
